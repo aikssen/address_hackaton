ec25ba9861396e24bfcc1d4ca6e291c86ae86946
Ricardo Luis Mender <rlmender@gmail.com>
2017-06-23 14:00:24 -0300

    Added new worker in application

--- address_validator/apps/address_validator_api/lib/address_validator_api/application.ex
+++ address_validator/apps/address_validator_api/lib/address_validator_api/application.ex
@@ -11,7 +11,7 @@ defmodule AddressValidatorApi.Application do
     # Define workers and child supervisors to be supervised
     children = [
       # Starts a worker by calling: AddressValidatorApi.Worker.start_link(arg1, arg2, arg3)
-      # worker(AddressValidatorApi.Worker, [arg1, arg2, arg3]),
+      worker(AddressValidatorApi.Server, []),
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
