d8a37e841ffba6b75deae53bb641939b9c58d965
Ever Cifuentes <ever.cifuentes@globant.com>
2017-06-27 14:40:06 -0500

    create production file for environments

---
+++ address_validator/apps/address_validator_api/config/prod.exs
@@ -0,0 +1,15 @@
+use Mix.Config
+
+config :address_validator_api, google_api_key: "AIzaSyCkwyRpvYKMnWX0JoR7VoO4oDNUhI5UWJQ"
+config :address_validator_api, google_api_url: "https://maps.googleapis.com/maps/api/geocode/json"
+
+
+# Configure your database
+config :address_validator_api, AddressValidatorApi.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  database: "address_validator_prod",
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  port: 5432,
+  pool_size: 
