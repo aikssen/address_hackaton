ed48a633137e2a80a416db087fef45dddc5cc696
Ever Cifuentes <ever.cifuentes@globant.com>
2017-06-27 14:17:50 -0500

    fix mix config for migrations

--- address_validator/apps/address_validator_api/config/config.exs
+++ address_validator/apps/address_validator_api/config/config.exs
@@ -28,8 +28,8 @@ use Mix.Config
 # here (which is why it is important to import them last).
 #
 #     import_config "#{Mix.env}.exs"
-config :address_validator_api, google_api_key: "AIzaSyCkwyRpvYKMnWX0JoR7VoO4oDNUhI5UWJQ"
-config :address_validator_api, google_api_url: "https://maps.googleapis.com/maps/api/geocode/json"
+# config :address_validator_api, google_api_key: "AIzaSyCkwyRpvYKMnWX0JoR7VoO4oDNUhI5UWJQ"
+# config :address_validator_api, google_api_url: "https://maps.googleapis.com/maps/api/geocode/json"
 
 # Configure your database
 config :address_validator_api, AddressValidatorApi.Repo,
@@ -40,3 +40,5 @@ config :address_validator_api, AddressValidatorApi.Repo,
   hostname: "localhost",
   port: 5432,
   pool_size: 10
+
+config :address_validator_api, ecto_repos: [AddressValidatorApi.Repo]
